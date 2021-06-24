52121ba745c98389677f88b2bfc16fd6d0d0db53
Lucia Seggiaro <luciaseggiaro@gmail.com>
2021-06-23 22:41:48 -0300

    general styling

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,12 +2,14 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
+    style: 'mapbox://styles/mapbox/dark-v10', // style URL
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 10, // starting zoom
 });
 
-new mapboxgl.Marker()
+map.addControl(new mapboxgl.NavigationControl());
+
+new mapboxgl.Marker({ color: '#52796f' })
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
